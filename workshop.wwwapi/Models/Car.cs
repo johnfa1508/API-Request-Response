04fc8d60df92110ab2467d68c07de0271ee357a8
gremble0 <hermanstornes@gmail.com>
2024-08-29 11:23:45 +0200

    General improvements

--- workshop.wwwapi/Models/Car.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace workshop.wwwapi.Models
-{
-    public class Car
-    {
-        public int Id { get; set; }
-        public string Make { get; set; }
-        public string Model { get; set; }
-    }
-}
